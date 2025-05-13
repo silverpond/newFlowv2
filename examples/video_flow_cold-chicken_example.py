0ae77891b3aed7abf74fad6f87b053fc0fcf4ff3
vtank4 <vietanhvt2004@gmail.com>
2025-05-13 18:00:18 +1000

    chore: add run example for chicken vid and write to json file

--- examples/video_flow_cold-chicken_example.py
+++ examples/video_flow_cold-chicken_example.py
@@ -1,14 +1,20 @@
+import time
 import cv2
 import numpy as np
 from neuflowv2 import OpticalFlow
+import json
 
 def main():
     # Initialize the OpticalFlow class
     # Change the model path as needed for your environment
     flow = OpticalFlow(model_path="models/neuflow_sintel.onnx")
     
+    #movement score
+    movement_scores: list[float] = []
+    
+    
     # Path to the input video file
-    video_path = "vendor/optical_flow_measure/inputs/test.mp4"
+    video_path = "vendor/optical_flow_measure/inputs/cold-chickens-combined.mp4"
     
     # Open the video file
     cap = cv2.VideoCapture(video_path)
@@ -27,6 +33,9 @@ def main():
     
     paused = False
     
+    # list to store all the flow vectors and movement scores and inference times 
+    frame_data: list[dict] = []
+    
     while True:
         if not paused:
             # Read a frame
@@ -34,12 +43,32 @@ def main():
             if not ret:
                 print("End of video stream.")
                 break
+            #time before inference
+            start = time.perf_counter()
             
             # Calculate flow vector
             flow_vectors = flow.update(frame)
             
+            #inference time for model to compute the flow vector of two consecutive frames
+            inference_time = (time.perf_counter() - start) * 1000 #milisecs
+            
             # Print the flow vector to the console
             print(f"Flow vector: {flow_vectors}")
+            
+            # Calculate movement score
+            movement_score = flow.compute_movement_score(flow_vectors)
+            movement_scores.append(float(movement_score))       
+            
+            #Store data for each process
+            data = {
+                "inference_time": inference_time,
+                "flow_vectors": flow_vectors.tolist(),
+                "movement_scores": movement_scores
+            }
+            
+            #save data to the list
+            frame_data.append(data)
+            
             
      #        # Display the flow vector on the frame
      #        cv2.putText(
@@ -54,6 +83,8 @@ def main():
         
      #    # Display the frame
      #    cv2.imshow("Optical Flow Movement", frame)
+     
+     
         
         # Handle key presses
         key = cv2.waitKey(1) & 0xFF
@@ -62,10 +93,14 @@ def main():
         elif key == ord('p'):
             paused = not paused
             print("Video paused" if paused else "Video resumed")
+            
+    #save data to json file
+    with open("flow_vectors_cold_chicken.json", "w") as f:
+        json.dump(frame_data, f)
     
     # Release resources
     cap.release()
-#     cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
     flow.reset()
 
 if __name__ == "__main__":
---
+++ examples/video_flow_hot-chicken_example.py
@@ -0,0 +1,107 @@
+import time
+import cv2
+import numpy as np
+from neuflowv2 import OpticalFlow
+import json
+
+def main():
+    # Initialize the OpticalFlow class
+    # Change the model path as needed for your environment
+    flow = OpticalFlow(model_path="models/neuflow_sintel.onnx")
+    
+    #movement score
+    movement_scores: list[float] = []
+    
+    
+    # Path to the input video file
+    video_path = "vendor/optical_flow_measure/inputs/hot-chickens-combined.mp4"
+    
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+    
+    if not cap.isOpened():
+        print(f"Error: Could not open video file {video_path}")
+        return
+    
+    # Get video properties
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    
+    print(f"Video loaded: {frame_width}x{frame_height} at {fps} FPS")
+    print("Press 'q' to quit, 'p' to pause/resume...")
+    
+    paused = False
+    
+    # list to store all the flow vectors and movement scores and inference times 
+    frame_data: list[dict] = []
+    
+    while True:
+        if not paused:
+            # Read a frame
+            ret, frame = cap.read()
+            if not ret:
+                print("End of video stream.")
+                break
+            #time before inference
+            start = time.perf_counter()
+            
+            # Calculate flow vector
+            flow_vectors = flow.update(frame)
+            
+            #inference time for model to compute the flow vector of two consecutive frames
+            inference_time = (time.perf_counter() - start) * 1000 #milisecs
+            
+            # Print the flow vector to the console
+            print(f"Flow vector: {flow_vectors}")
+            
+            # Calculate movement score
+            movement_score = flow.compute_movement_score(flow_vectors)
+            movement_scores.append(float(movement_score))       
+            
+            #Store data for each process
+            data = {
+                "inference_time": inference_time,
+                "flow_vectors": flow_vectors.tolist(),
+                "movement_scores": movement_scores
+            }
+            
+            #save data to the list
+            frame_data.append(data)
+            
+            
+        #     # Display the flow vector on the frame
+        #     cv2.putText(
+        #         frame, 
+        #         f"Flow vector: {flow_vectors}", 
+        #         (10, 30), 
+        #         cv2.FONT_HERSHEY_SIMPLEX, 
+        #         1, 
+        #         (0, 255, 0), 
+        #         2
+        #     )
+        
+        # # Display the frame
+        # cv2.imshow("Optical Flow Movement", frame)
+     
+     
+        
+        # Handle key presses
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+        elif key == ord('p'):
+            paused = not paused
+            print("Video paused" if paused else "Video resumed")
+            
+    #save data to json file
+    with open("flow_vectors_hot_chicken.json", "w") as f:
+        json.dump(frame_data, f)
+    
+    # Release resources
+    cap.release()
+    #cv2.destroyAllWindows()
+    flow.reset()
+
+if __name__ == "__main__":
+    main() 
